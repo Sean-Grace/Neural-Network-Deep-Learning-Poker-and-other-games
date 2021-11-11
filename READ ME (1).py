070c09eff2a4683c4c0efd9f752b916922728637
Sean-Grace <117419592@umail.ucc.ie>
2021-11-11 14:42:14 +0000

    Delete READ ME (1).py

--- READ ME (1).py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# The goal of this project is to find optimum stradegys for imperfect information chance based games. There are two examples so far Rock, Paper, Scissors and a modified Poker.
-# The RPS is self explanatory with a reward of 1 given for a win and -1 for a loss too each player.
-# The Basic Poker plays out as so: There is an initial round of betting of one to enter the game, and one round of actual betting. The players are given choice to bet or pass which is the b and p in the results. A simplification i made to the game was I carried out the process only caring about hank ranking i.e high card, two pair, 3 of a kind and so on totaling 10 different types of hands.
-# I will later update with all available hands.
-# 
